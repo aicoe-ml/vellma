aa7e82901efe975b0533e1c422b0ba24116f9684
Jess Alfredsen <jess@planmate.ai>
2024-07-30 10:28:38 +0200

    Disabling chats history in Admin panel.

--- src/routes/(app)/admin/+page.svelte
+++ src/routes/(app)/admin/+page.svelte
@@ -308,12 +308,11 @@
 						<td class="px-3 py-2 text-right">
 							<div class="flex justify-end w-full">
 								{#if user.role !== 'admin'}
-									<Tooltip content={$i18n.t('Chats')}>
+									<Tooltip content={$i18n.t('Chats is disabled in Velliv OpenWebUI')}>
 										<button
 											class="self-center w-fit text-sm px-2 py-2 hover:bg-black/5 dark:hover:bg-white/5 rounded-xl"
 											on:click={async () => {
-												showUserChatsModal = !showUserChatsModal;
-												selectedUser = user;
+												alert('Chats is disabled in Velliv OpenWebUI')
 											}}
 										>
 											<ChatBubbles />
